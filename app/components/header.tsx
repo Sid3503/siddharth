d47cffcba7d52d59a01472274655c465cf25a921
Siddharth Mishra <120008126+Sid3503@users.noreply.github.com>
2025-04-10 17:15:16 +0530

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -13,15 +13,15 @@ export default function Header() {
           <Mail size={18} />
           <span>mishrasiddharth072gmail.com</span>
         </a>
-        <a href="#" className="flex items-center gap-2">
+        <a href="https://www.linkedin.com/in/siddharth-mishra-0a5227228/" className="flex items-center gap-2">
           <Linkedin size={18} />
           <span>LinkedIn</span>
         </a>
-        <a href="#" className="flex items-center gap-2">
+        <a href="https://github.com/Sid3503" className="flex items-center gap-2">
           <Github size={18} />
           <span>GitHub</span>
         </a>
-        <a href="#" className="flex items-center gap-2">
+        <a href="https://exuberant-week-19a.notion.site/Siddharth-Mishra-c534e96b354b45db87a6b45ee22cf90e?pvs=74" className="flex items-center gap-2">
           <span className="font-bold">Notion</span>
         </a>
       </div>
