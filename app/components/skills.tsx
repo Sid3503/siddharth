acce482749e8f51013ce3af68db668842d0a9938
Siddharth Mishra <120008126+Sid3503@users.noreply.github.com>
2025-06-24 00:11:47 +0530

    Update skills.tsx

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -12,7 +12,6 @@ export default function Skills() {
 
   return (
     <section className="mb-8">
-      <h2 className="text-2xl font-bold mb-4">Skill Set</h2>
       <ul className="list-disc pl-5 grid grid-cols-2 gap-2">
         {skills.map((skill, index) => (
           <li key={index}>{skill}</li>
